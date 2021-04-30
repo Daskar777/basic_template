b8edda9e9f3ccd31fc26cc724f43c41c01c2c71e
Daskar <winworf@ukr.net>
2021-04-30 17:29:59 +0300

    V 1.1 style.sass + style.css

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gulp      	= require('gulp'), // Подключаем Gulp
 gulp.task('sass', function(){ // Создаем таск Sass
     return gulp.src('app/sass/**/*.sass') // Берем источник
         .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
-        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+        .pipe(gulp.dest('app')) // Выгружаем результата в папку app
         .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
 
